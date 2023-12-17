38f1349868a23e6f930af3b9d57a83a0757044d1
Louai-AZ <azouni.louai@gmail.com>
2023-12-17 19:38:37 +0100

    Updates

--- Laboratoire/LaboratoireInterface.py
+++ Laboratoire/LaboratoireInterface.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtCore import pyqtSignal
 import psycopg2
 from AjouterLaboratoireDialog import AjouterLaboratoireDialog
+from Laboratoire.AfficherHierarchieDialog import AfficherHierarchieDialog
 
 class LaboratoireInterface(QMainWindow):
 
